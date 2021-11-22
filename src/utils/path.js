7bb71209193fd3bc6c731cdcbcfe4f38cca63f5e
Ученик <8887081-allyru0@users.noreply.gitlab.com>
2021-11-22 21:05:20 +0300

    start project

---
+++ src/utils/path.js
@@ -0,0 +1,12 @@
+import AuthPage from "../components/pages/AuthPage/AuthPage";
+
+
+export const AUTH_ROUTE = '/home'
+
+
+export const routes = [
+    {
+        path: AUTH_ROUTE,
+        component: AuthPage,
+    }
+]
